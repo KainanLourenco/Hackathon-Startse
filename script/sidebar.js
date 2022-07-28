b716eeccfe266f326195d77db513d7dd373058b6
Kauã Lourenço <kaualourenco25@gmail.com>
2022-07-27 21:13:18 -0300

    linked pages and other minor changes

---
+++ script/sidebar.js
@@ -0,0 +1,15 @@
+const sidebar = document.querySelector('.sidebar');
+const openSide = document.querySelector('nav ul li:last-child');
+const closeSide = document.querySelector('.closebtn');
+
+openSide.addEventListener('click', () => {
+    if (sidebar.style.left === '0px') {
+        sidebar.style.left = '-100%';
+    } else {
+        sidebar.style.left = '0px';
+    }
+});
+
+closeSide.addEventListener('click', () => {
+    sidebar.style.left = '-100%';
+});
